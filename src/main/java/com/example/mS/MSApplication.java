521eab161d9f3ea411bad516b2ac519cd1903ea6
Naga Kiran Kumar Yanike <naga.kiran.kumar.yanike@ericsson.com>
2024-02-24 15:57:23 -0500

    Initial commit

---
+++ src/main/java/com/example/mS/MSApplication.java
@@ -0,0 +1,13 @@
+package com.example.mS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSApplication.class, args);
+	}
+
+}
